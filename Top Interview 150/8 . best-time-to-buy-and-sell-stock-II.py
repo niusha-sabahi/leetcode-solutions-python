2471e9355052bc6d7be370186190228ecc6b486b
Niush0110 <niushaesabahi@gmail.com>
2023-11-14 21:23:39 +0000

    Memory optimised, maintained high runtime speed

--- Top Interview 150/8 . best-time-to-buy-and-sell-stock-II.py
+++ Top Interview 150/8 . best-time-to-buy-and-sell-stock-II.py
@@ -4,22 +4,14 @@
 # your stock on the same day and vice versa. If you cannot achieve any profit, return 0.
 
 # -- Performance At Time Of Completion --
-# Runtime : Beats % of users with Python3 - GREEN
-# Memory : Beats % of users with Python3 - GREEN
+# Runtime : Beats 94.74% of users with Python3 - GREEN
+# Memory : Beats 65.15% of users with Python3 - GREEN
 
 def maxProfit(prices):
-    if not prices or len(prices) == 1:
-        return 0
-
-    buy_price = prices[0]
     total_profit = 0
 
-    for i in range (1, len(prices)):
-        if prices[i] < buy_price:
-            buy_price = prices[i]
-        else:
-            total_profit = total_profit + (prices[i] - buy_price)
-            buy_price = prices[i]
+    for i in range (len(prices)-1):
+        total_profit += max( prices[i+1] - prices[i], 0)
     return total_profit
 
 print(maxProfit([1,2,3,4,5])) # expected output : 4
